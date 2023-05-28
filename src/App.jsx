5534856643549040dcd43fcd2849149e11cf179c
macbook kal <arnaud.pantenier@gmail.com>
2023-05-28 21:59:09 +0200

    Ajout fetch api + debut Connect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 import "./sass/style.scss";
-import Header from "./components/Header"
+import Header from "./components/Header";
+import Connect from "./components/Connect";
+import { useState } from "react";
 
 function App() {
+    const [usersList, setUsersList] = useState(null);
+    const [userConnect, setUserConnect] = useState(false);
+
+    // FUNCTION APPEL A API
+    async function callAPI(url) {
+        try {
+            const user = await fetch(url);
+            const dataJson = await user.json();
+            setUsersList(dataJson);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    usersList === null && callAPI("./src/api/users.json");
+
+    //console.log(usersList.users);
 
     return (
         <>
             <Header />
-            
+            {
+              !userConnect && <Connect setUserConnect={setUserConnect} />
+            }
         </>
     );
 }
 
 export default App;
-
--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -1,8 +1,15 @@
 
 const Connect = () => {
     return (
-        <div>
-            
+        <div className="box-connect">
+            <form className="form-login">
+                <label htmlFor="login">Login</label>
+                <input type="text" id="code" />
+                <label htmlFor="password" id="code">Code à 3 chiffres</label>
+                <input type="number" name="passcode" id="code" />
+                <button type="submit">VALIDER</button>
+                <p>Pas de compte ? inscrivez-vous</p>
+            </form>
         </div>
     );
 };
