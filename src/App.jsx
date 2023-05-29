86c18790afce620152da9f8705524bb91128317e
macbook kal <arnaud.pantenier@gmail.com>
2023-05-29 22:24:03 +0200

    Ajout localhost

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import Connect from "./components/Connect";
 import { useState } from "react";
 
 function App() {
+
     const [usersList, setUsersList] = useState(null);
     const [userConnect, setUserConnect] = useState(false);
-
+    
     // FUNCTION APPEL A API
     async function callAPI(url) {
         try {
             const user = await fetch(url);
             const dataJson = await user.json();
             setUsersList(dataJson);
+            localStorage.setItem("usersList", JSON.stringify(dataJson))
+
         } catch (err) {
             console.log(err);
         }
@@ -20,14 +23,11 @@ function App() {
 
     usersList === null && callAPI("./src/api/users.json");
 
-    //console.log(usersList.users);
 
     return (
         <>
             <Header />
-            {
-              !userConnect && <Connect setUserConnect={setUserConnect} />
-            }
+            {!userConnect && <Connect setUserConnect={setUserConnect} usersList={usersList} />}
         </>
     );
 }
--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -1,14 +1,25 @@
-
-const Connect = () => {
+const Connect = ({ setUserConnect, usersList }) => {
     return (
         <div className="box-connect">
             <form className="form-login">
                 <label htmlFor="login">Login</label>
-                <input type="text" id="code" />
-                <label htmlFor="password" id="code">Code à 3 chiffres</label>
-                <input type="number" name="passcode" id="code" />
+                <input type="text" id="code" required />
+                <label htmlFor="password" id="code">
+                    Code à 3 chiffres
+                </label>
+                <input
+                    type="password"
+                    name="passcode"
+                    id="code"
+                    minLength="3"
+                    maxLength="3"
+                    inputMode="numeric"
+                    required
+                />
                 <button type="submit">VALIDER</button>
-                <p>Pas de compte ? inscrivez-vous</p>
+                <p>
+                    Pas de compte ? <span>inscrivez-vous</span>
+                </p>
             </form>
         </div>
     );
