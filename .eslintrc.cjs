731cc32f2dd16dd7935ffd31ec9fbd50727304cc
macbook kal <arnaud.pantenier@gmail.com>
2023-07-06 16:22:07 +0200

    Suppression erreur module dans eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,16 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+  "globals": { "angular": false, "module": false, "inject": false, "document": false }, 
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+	],
+	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+	settings: { react: { version: '18.2' } },
+	plugins: ['react-refresh'],
+	rules: {
+		'react-refresh/only-export-components': 'warn',
+	},
+};
--- src/components/ModalBottle.jsx
+++ src/components/ModalBottle.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
 const ModalBottle = ({ children, closeModal, activeModal }) => {
-
-    if (!activeModal) return null
+    if (!activeModal) return null;
 
     return (
         <div className="rack-modal" onClick={() => closeModal()}>
