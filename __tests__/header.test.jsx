d8fe33f6c83803a893c40ecae67665d7e08080a8
macbook kal <arnaud.pantenier@gmail.com>
2023-08-21 23:05:30 +0200

    Tests pour header

--- __tests__/header.test.jsx
+++ __tests__/header.test.jsx
@@ -1,9 +1,21 @@
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
-import '@testing-library/jest-dom'
+import "@testing-library/jest-dom";
 import Header from "../src/components/Header";
 
-test("devrait afficher le contenu attendu", () => {
+test("Affiche le titre H1", () => {
     render(<Header />);
-    
-  });
+    const title = screen.getByText("RACKS A PINARD");
+});
+
+test("User not connect => Affiche inscription", () => {
+    render(<Header connect={false} />);
+    const inscription = screen.getByText("Inscription");
+    expect(inscription).toBeInTheDocument();
+});
+
+test("User connect => N'affiche pas inscription", () => {
+    render(<Header connect={true} />);
+    const inscription = screen.queryByText("Inscription");
+    expect(inscription).not.toBeInTheDocument();
+});
