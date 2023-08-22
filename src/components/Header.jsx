763c1730024e9fa3229b459b139f03123a21919b
macbook kal <arnaud.pantenier@gmail.com>
2023-08-22 22:51:34 +0200

    Ajout modal inscription fail

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
+import ModalConnect from "./ModalConnect";
 
 const Header = ({ connect }) => {
+    const [showModal, setShowModal] = useState(false);
     return (
         <header>
-            <h1 aria-level="1" >RACKS A PINARD</h1>
-            {!connect && <p>Inscription</p>}
+            <h1>RACKS A PINARD</h1>
+            {!connect && 
+                <div 
+                    className="btn-inscription"
+                    onClick={() => setShowModal(true)}
+                    >Inscription</div>}
+            {showModal && createPortal(<ModalConnect setShowModal={setShowModal} />, document.body)}
         </header>
     );
 };
---
+++ src/components/ModalConnect.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ModalConnect = ({ setShowModal }) => {
+
+    return (
+        <div className="layout-modal-inscription-fail" onClick={() => setShowModal(false)}>
+            <div className="modal-inscription-fail" onClick={e => e.stopPropagation()}>
+                <h2>See you soon</h2>
+                <p>Les inscriptions ne sont pas encore ouvertes</p>
+                <button onClick={() => setShowModal(false)}>X</button>
+            </div>
+        </div>
+    );
+};
+
+export default ModalConnect;
