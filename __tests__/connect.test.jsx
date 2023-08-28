1bf1015323cb5bc311088b61683c7134a011ab4d
macbook kal <arnaud.pantenier@gmail.com>
2023-08-28 16:16:15 +0200

    Ajout tests modal + mode demo dans Connect

--- __tests__/connect.test.jsx
+++ __tests__/connect.test.jsx
@@ -4,7 +4,7 @@ import "@testing-library/jest-dom";
 import Connect from "../src/components/Connect";
 
 describe("Tests for the Connect component", () => {
-    test("render du component", () => {
+    test("render du component Connect", () => {
         render(<Connect />);
     });
 
@@ -27,7 +27,7 @@ describe("Tests for the Connect component", () => {
         render(<Connect dispatch={mockDispatch} />);
         const inputLogin = screen.getByLabelText("Login");
         const inputPassword = screen.getByTestId("password");
-        const btnSubmit = screen.getByRole("button");
+        const btnSubmit = screen.getByText(/VALIDER/i);
         fireEvent.change(inputLogin, { target: { value: "loginOfUser" } });
         fireEvent.change(inputPassword, { target: { value: 123 } });
         fireEvent.click(btnSubmit);
@@ -39,16 +39,36 @@ describe("Tests for the Connect component", () => {
 
     test("Affiche message erreur au submit si error", () => {
         render(<Connect messageError={true} />);
-        const msgError = screen.getByText(/Login ou code incorrect/i)
-        const btnSubmit = screen.getByRole("button");
+        const msgError = screen.getByText(/Login ou code incorrect/i);
+        const btnSubmit = screen.getByText(/VALIDER/i);
         fireEvent.click(btnSubmit);
-        expect(msgError).toBeInTheDocument()
+        expect(msgError).toBeInTheDocument();
     });
 
     test("Affiche Pas de compte si pas de error", () => {
         render(<Connect messageError={false} />);
-        const msgInscription = screen.getByText(/Inscrivez-vous/i)
-        expect(msgInscription).toBeInTheDocument()
-    })
+        const msgInscription = screen.getByText(/Inscrivez-vous/i);
+        expect(msgInscription).toBeInTheDocument();
+    });
 
+    test("Affiche modal d'inscription", () => {
+        const mockModalInscription = vi.fn();
+        render(<Connect setShowModal={mockModalInscription} />);
+        const btnInscription = screen.getByText(/Pas de compte ?/i);
+        fireEvent.click(btnInscription);
+        expect(mockModalInscription).toHaveBeenCalledWith(true)
+    });
+
+    test("Activation version de demonstration", () => {
+        const mockSubmitDemo = vi.fn();
+        render(<Connect dispatch={mockSubmitDemo} />)
+        const btnDemo = screen.getByText(/Version de démonstration/i)
+        fireEvent.click(btnDemo)
+        expect(mockSubmitDemo).toHaveBeenCalledWith(
+            {
+                type: "verified_ident",
+                payload: { userLogin: "demo", code: 123 },
+            }
+        )
+    })
 });
